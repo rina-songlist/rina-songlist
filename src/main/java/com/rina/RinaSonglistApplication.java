5ba251f0e85191ec30abd9530fa4b9ab15f0752e
ArvinXY <arvinjunior@163.com>
2022-02-06 01:39:43 +0900

    initial commit

---
+++ src/main/java/com/rina/RinaSonglistApplication.java
@@ -0,0 +1,13 @@
+package com.rina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinaSonglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinaSonglistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rina/RinaSonglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinaSonglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
