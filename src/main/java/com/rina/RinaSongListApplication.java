7a5c4ef78ec353ba7927d7f9265a10560b123b4e
ArvinXY <arvinjunior@163.com>
2022-02-07 15:51:06 +0900

    refactor(environment): 更改了主启动项
    更改了主启动项名

--- src/main/java/com/rina/RinaSongListApplication.java
+++ src/main/java/com/rina/RinaSongListApplication.java
@@ -1,13 +1,17 @@
 package com.rina;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author arvin
+ */
 @SpringBootApplication
 public class RinaSonglistApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RinaSonglistApplication.class, args);
+		SpringApplication.run(RinaSongListApplication.class, args);
 	}
 
 }
--- src/test/java/com/rina/RinaSongListApplicationTests.java
+++ src/test/java/com/rina/RinaSongListApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RinaSonglistApplicationTests {
+class RinaSongListApplicationTests {
 
 	@Test
 	void contextLoads() {
