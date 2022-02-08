a0b3892484f0a749adc4d802c33513152b00e3d4
ArvinXY <arvinjunior@163.com>
2022-02-09 05:22:59 +0900

    test(mapper): 添加了mapper的测试类
    添加了mapper的测试类

---
+++ src/test/java/com/rina/mapper/SongListMapperTests.java
@@ -0,0 +1,33 @@
+package com.rina.mapper;
+
+import com.rina.domain.SongList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * mapper的测试类
+ * @author arvin
+ * @date 2022/02/07
+ */
+@SpringBootTest
+public class SongListMapperTests {
+
+	@Autowired
+	private SongListMapper songListMapper;
+
+	@Test
+	public void selectByNameOrArtist() {
+		final List<SongList> songList = songListMapper.selectByNameOrArtist("影炎");
+		System.out.println(songList);
+	}
+
+	@Test
+	public void selectByIds() {
+		final List<SongList> songList = songListMapper.selectByIds(new Long[]{50L, 40L});
+		System.out.println(songList);
+	}
+
+}
