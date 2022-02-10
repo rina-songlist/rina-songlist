4e7da6873b10ce23356ff23007dad2c39b2f8931
ArvinXY <arvinjunior@163.com>
2022-02-10 14:42:56 +0900

    feat(interceptor): 添加了拦截器
    添加了用户信息拦截器

---
+++ src/main/java/com/rina/interceptor/AuthInfoInterceptor.java
@@ -0,0 +1,68 @@
+package com.rina.interceptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rina.enums.ResultCode;
+import com.rina.resp.Resp;
+import com.rina.util.JwtUtil;
+import com.rina.util.MdcScope;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 用户信息拦截器
+ *
+ * @author arvin
+ * @date 2022/02/09
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthInfoInterceptor implements HandlerInterceptor {
+
+	private final JwtUtil jwtUtil;
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json; charset=UTF-8");
+
+		final String token = request.getHeader("Authorization");
+
+		final String responseBody = objectMapper.writeValueAsString(Resp.failed(ResultCode.UNAUTHORIZED));
+
+		try (MdcScope mdcScope = new MdcScope()) {
+			if (null != token) {
+				if (jwtUtil.validateToken(token)) {
+					// 解析出当前用户的简要信息
+					final String roleName = jwtUtil.parseClaims(token)
+							.map(Claims::getSubject)
+							.orElse(null);
+					final String userName = jwtUtil.parseClaims(token)
+							.map(claims -> claims.get("authorities", String.class))
+							.orElse(null);
+
+					// 把用户信息写入MDC
+					mdcScope.put("roleName", roleName);
+					mdcScope.put("userName", userName);
+					log.info("当前用户信息已写入MDC");
+					return true;
+				}
+			} else {
+				log.error("当前用户未携带token");
+			}
+		} catch (Exception e) {
+			log.error("MDC注入发生错误 {}", e.getLocalizedMessage());
+		}
+
+		response.getWriter().append(responseBody);
+		return false;
+	}
+}
