6cb531ce93af33928bd693db6b4ed18a58e85e92
ArvinXY <arvinjunior@163.com>
2022-02-10 14:49:22 +0900

    feat(mapper): 添加了mapper
    添加了role表所对应的mapper

---
+++ src/main/java/com/rina/mapper/RoleMapper.java
@@ -0,0 +1,24 @@
+package com.rina.mapper;
+
+import com.rina.domain.Role;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * role表所对应的mapper
+ *
+ * @author arvin
+ */
+@Mapper
+public interface RoleMapper {
+    int deleteByPrimaryKey(Long roleId);
+
+    int insert(Role record);
+
+    Role selectByPrimaryKey(Long roleId);
+
+    List<Role> selectAll();
+
+    int updateByPrimaryKey(Role record);
+}
