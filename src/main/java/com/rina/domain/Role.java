98d3b51141c050185d353c6de59dfa7c949556a8
ArvinXY <arvinjunior@163.com>
2022-02-10 14:48:22 +0900

    feat(domain): 添加了实体类
    添加了role表所对应的实体类

---
+++ src/main/java/com/rina/domain/Role.java
@@ -0,0 +1,34 @@
+package com.rina.domain;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * role表所对应的实体类
+ *
+ * @author arvin
+ */
+@Data
+@Builder
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 8847562476138143980L;
+
+    private Long roleId;
+
+    private String roleName;
+
+    private String createBy;
+
+    private Date createTime;
+
+    private String updateBy;
+
+    private Date updateTime;
+
+}
