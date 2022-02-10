01acfea245710152b93c2a93b8a5b9d2e6de2222
ArvinXY <arvinjunior@163.com>
2022-02-10 14:48:37 +0900

    feat(domain): 添加了实体类
    添加了user表所对应的实体类

---
+++ src/main/java/com/rina/domain/User.java
@@ -0,0 +1,38 @@
+package com.rina.domain;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * user表所对应的实体类
+ *
+ * @author arvin
+ */
+@Data
+@Builder
+@With
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 8867247020450884661L;
+
+    private Long userId;
+
+    private String userName;
+
+    private String password;
+
+    private Boolean status;
+
+    private String createBy;
+
+    private Date createTime;
+
+    private String updateBy;
+
+    private Date updateTime;
+
+}
