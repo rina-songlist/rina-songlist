20ef36283420637d653223e5a016cf48eda0bf49
ArvinXY <arvinjunior@163.com>
2022-02-10 14:46:17 +0900

    feat(util): 添加了工具类
    添加了MDC的工具类

---
+++ src/main/java/com/rina/util/MdcScope.java
@@ -0,0 +1,44 @@
+package com.rina.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+
+import java.util.Map;
+
+/**
+ * 把用户信息通过MDC传递
+ *
+ * @author arvin
+ * @date 2022/02/09
+ */
+@Slf4j
+public class MdcScope implements AutoCloseable {
+
+	private final Map<String, String> location = MDC.getCopyOfContextMap();
+
+	@Override
+	public void close() throws Exception {
+		if (null != this.location) {
+			MDC.setContextMap(this.location);
+		} else {
+			MDC.clear();
+		}
+	}
+
+	/**
+	 * 将当前用户信息存入MDC
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public MdcScope put(String key, String value){
+		if (null != value) {
+			MDC.put(key, value);
+		} else if (null != MDC.get(key) && null != value) {
+			MDC.remove(key);
+			MDC.put(key, value);
+		}
+		log.error("当前key是空值！key为：" + key);
+		return this;
+	}
+}
