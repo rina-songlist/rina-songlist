e69f294037e71c7f809ba033d6b41e9d267e7be1
ArvinXY <arvinjunior@163.com>
2022-02-18 23:40:30 +0900

    feat(controller): 添加了控制类参数
    添加了排序依据和是否倒序排序的参数

--- src/main/java/com/rina/controller/SongListController.java
+++ src/main/java/com/rina/controller/SongListController.java
@@ -7,7 +7,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -29,9 +32,11 @@ public class SongListController {
 	@ApiOperation(value = "普通用户所看到的歌单信息", notes = "无需鉴权")
 	public PageResp<List<SongListDto>> publicUserSongList(@RequestParam(required = true) @ApiParam(value = "页面展示的数据量", required = true) Integer pageSize,
 	                                                      @RequestParam(required = true) @ApiParam(value = "当前页数", required = true) Integer pageNum,
-	                                                      @RequestParam(required = false) @ApiParam(value = "歌曲IDs", required = false) Long[] ids,
-	                                                      @RequestParam(required = false) @ApiParam(value = "歌名或歌手名", required = false) String nameOrArtist) {
-		return songListService.querySongLists(pageSize, pageNum, ids, nameOrArtist);
+														  @RequestParam(required = true) @ApiParam(value = "排序依据", required = true) String orderBy,
+														  @RequestParam(required = true) @ApiParam(value = "是否倒序排序", required = true) Boolean withDesc,
+	                                                      @RequestParam(required = false) @ApiParam(value = "歌名或歌手名", required = false) String nameOrArtist,
+	                                                      @RequestParam(required = false) @ApiParam(value = "歌曲IDs", required = false) Long... ids) {
+		return songListService.querySongLists(pageSize, pageNum, orderBy, withDesc, nameOrArtist, ids);
 	}
 
 }
