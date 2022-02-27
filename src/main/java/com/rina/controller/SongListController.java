5d325b9813b44d71c9d437fe922a4f282028fe90
ArvinXY <arvinjunior@163.com>
2022-02-28 08:01:37 +0900

    fix(controller): 更改了controller相关
    修复了无法复用父类功能的问题

--- src/main/java/com/rina/controller/SongListController.java
+++ src/main/java/com/rina/controller/SongListController.java
@@ -1,7 +1,6 @@
 package com.rina.controller;
 
-import com.rina.domain.dto.SongListDto;
-import com.rina.resp.PageResp;
+import com.rina.resp.Resp;
 import com.rina.service.SongListService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * 歌单展示相关的控制器
@@ -30,12 +27,12 @@ public class SongListController {
 
 	@GetMapping("/public-list")
 	@ApiOperation(value = "普通用户所看到的歌单信息", notes = "无需鉴权")
-	public PageResp<List<SongListDto>> publicUserSongList(@RequestParam(required = true) @ApiParam(value = "页面展示的数据量", required = true) Integer pageSize,
-	                                                      @RequestParam(required = true) @ApiParam(value = "当前页数", required = true) Integer pageNum,
-														  @RequestParam(required = true) @ApiParam(value = "排序依据", required = true) String orderBy,
-														  @RequestParam(required = true) @ApiParam(value = "是否倒序排序", required = true) Boolean withDesc,
-	                                                      @RequestParam(required = false) @ApiParam(value = "歌名或歌手名", required = false) String nameOrArtist,
-	                                                      @RequestParam(required = false) @ApiParam(value = "歌曲IDs", required = false) Long... ids) {
+	public Resp publicUserSongList(@RequestParam(required = true) @ApiParam(value = "页面展示的数据量", required = true) Integer pageSize,
+	                               @RequestParam(required = true) @ApiParam(value = "当前页数", required = true) Integer pageNum,
+	                               @RequestParam(required = true) @ApiParam(value = "排序依据", required = true) String orderBy,
+	                               @RequestParam(required = true) @ApiParam(value = "是否倒序排序", required = true) Boolean withDesc,
+	                               @RequestParam(required = false) @ApiParam(value = "歌名或歌手名", required = false) String nameOrArtist,
+	                               @RequestParam(required = false) @ApiParam(value = "歌曲IDs", required = false) Long... ids) {
 		return songListService.querySongLists(pageSize, pageNum, orderBy, withDesc, nameOrArtist, ids);
 	}
 
