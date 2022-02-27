ca9b328a4442774d09b90572970315655cfdd183
ArvinXY <arvinjunior@163.com>
2022-02-28 08:03:47 +0900

    feat(DTO): 添加了dto
    添加了用户登陆时的请求体

---
+++ src/main/java/com/rina/domain/dto/UserLoginDto.java
@@ -0,0 +1,37 @@
+package com.rina.domain.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * 用户登陆时的请求体
+ *
+ * @author arvin
+ * @date 2022/02/28
+ */
+@Data
+@Builder
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(description = "登陆信息")
+public class UserLoginDto implements Serializable {
+
+	private static final long serialVersionUID = -2438647957404692821L;
+
+	/**
+	 * 用户名
+	 */
+	@ApiModelProperty(value = "用户名", required = true, position = 1)
+	private String username;
+
+	/**
+	 * 密码
+	 */
+	@ApiModelProperty(value = "密码", required = true, position = 2)
+	private String password;
+
+}
