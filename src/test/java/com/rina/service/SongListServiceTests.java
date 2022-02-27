6995dc2ea1d6e9731cdad311fc7c2b44e77f6acd
ArvinXY <arvinjunior@163.com>
2022-02-28 07:47:30 +0900

    style(tests): 更改了测试相关
    规范化测试方法名
    
    feat(tests): 更改了测试相关
    
    更改了返回体类型

--- src/test/java/com/rina/service/SongListServiceTests.java
+++ src/test/java/com/rina/service/SongListServiceTests.java
@@ -2,13 +2,10 @@ package com.rina.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rina.domain.dto.SongListDto;
-import com.rina.resp.PageResp;
+import com.rina.resp.Resp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 /**
  * 歌单展示相关的服务的测试类
@@ -26,15 +23,15 @@ public class SongListServiceTests {
 	private ObjectMapper objectMapper;
 
 	@Test
-	void TestQuerySongListsWithNameAndArtist() throws JsonProcessingException {
-		final PageResp<List<SongListDto>> listPageResp = songListService.querySongLists(10, 1, "id", false,null,null);
+	void testQuerySongListsWithNameAndArtist() throws JsonProcessingException {
+		final Resp listPageResp = songListService.querySongLists(10, 1, "id", false,null,null);
 		final String s = objectMapper.writeValueAsString(listPageResp);
 		System.out.println(s);
 	}
 
 	@Test
-	void TestQuerySongListsWithIds() throws JsonProcessingException {
-		final PageResp<List<SongListDto>> listPageResp = songListService.querySongLists(10, 1, "name", true,null, new Long[]{50L, 40L});
+	void testQuerySongListsWithIds() throws JsonProcessingException {
+		final Resp listPageResp = songListService.querySongLists(10, 1, "name", true,null, 50L, 40L);
 		final String s = objectMapper.writeValueAsString(listPageResp);
 		System.out.println(s);
 	}
