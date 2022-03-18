e93de9cd02f4b302d41cbabb009fb78f5cfdbf14
ArvinXY <arvinjunior@163.com>
2022-03-18 23:56:39 +0900

    feat(config): 更改了SwaggerUI配置类
    添加了认证功能

--- src/main/java/com/rina/config/SwaggerConfig.java
+++ src/main/java/com/rina/config/SwaggerConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * SwaggerUI配置类
@@ -27,11 +30,34 @@ public class SwaggerConfig {
 	@Bean
 	public Docket createRestApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
 				.apiInfo(apiInfo())
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.rina.controller"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.securitySchemes(securitySchemes())
+				.securityContexts(securityContexts());
+	}
+
+	private List<SecurityScheme> securitySchemes() {
+		return Collections.singletonList(new ApiKey("Authorization", "Authorization", "header"));
+	}
+
+	private List<SecurityContext> securityContexts() {
+		return Collections.singletonList(
+				SecurityContext.builder()
+						.securityReferences(defaultAuth())
+						.forPaths(PathSelectors.ant("/private/system/*"))
+						.build()
+		);
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		final AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		final AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Collections.singletonList(new SecurityReference("Authorization", authorizationScopes));
 	}
 
 	/**
