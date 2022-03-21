6609f24aebc3a2a5affeaa0ae34b7c058b457b82
ArvinXY <arvinjunior@163.com>
2022-03-22 04:00:34 +0900

    feat(mapper): 添加了menu表所对应的mapper
    添加了menu表所对应的mapper

---
+++ src/main/java/com/rina/mapper/MenuMapper.java
@@ -0,0 +1,31 @@
+package com.rina.mapper;
+
+import com.rina.domain.Menu;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * menu表所对应的mapper
+ *
+ * @author arvin
+ */
+@Mapper
+public interface MenuMapper {
+    int deleteByPrimaryKey(Long menuId);
+
+    int insert(Menu record);
+
+    Menu selectByPrimaryKey(Long menuId);
+
+    List<Menu> selectAll();
+
+    int updateByPrimaryKey(Menu record);
+
+    /**
+     * 通过菜单ID查询当前菜单
+     * @param menuId 菜单ID
+     * @return 当前菜单
+     */
+    Menu findMenuById(Long menuId);
+}
