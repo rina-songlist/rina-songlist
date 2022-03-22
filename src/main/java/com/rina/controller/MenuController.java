5f2640a060cb9bc61e1f18f9f5c90b41b9247d3d
ArvinXY <arvinjunior@163.com>
2022-03-23 00:49:56 +0900

    feat(controller): 更改了菜单所对应的controller
    添加了单一菜单的增删改查

--- src/main/java/com/rina/controller/MenuController.java
+++ src/main/java/com/rina/controller/MenuController.java
@@ -1,14 +1,13 @@
 package com.rina.controller;
 
+import com.rina.domain.dto.MenuDto;
 import com.rina.resp.Resp;
 import com.rina.service.MenuService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 菜单所对应的controller
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/private/system")
 @RequiredArgsConstructor
+@Api(tags = "菜单相关接口")
 public class MenuController {
 
 	private final MenuService menuService;
@@ -32,6 +32,24 @@ public class MenuController {
 			return menuService.listMenus();
 		}
 		return Resp.failed();
+	}
+
+	@GetMapping("/menu/{id}")
+	@ApiOperation(value = "获取单一菜单", notes = "需要授权")
+	public Resp getSingleMenu(@PathVariable(value = "id", required = true) @ApiParam(value = "查询单个菜单", required = true) Long menuId) {
+		return menuService.getSingleMenu(menuId);
+	}
+
+	@PutMapping("/menu")
+	@ApiOperation(value = "添加/编辑单一菜单", notes = "需要授权")
+	public Resp editMenu(@RequestBody(required = true) @ApiParam(value = "菜单详情", required = true) MenuDto menuDto) {
+		return menuService.editMenu(menuDto);
+	}
+
+	@DeleteMapping("/menu/{id}")
+	@ApiOperation(value = "删除菜单", notes = "需要授权")
+	public Resp deleteMenu(@PathVariable(value = "id", required = true) @ApiParam(value = "菜单ID", required = true) Long menuId) {
+		return menuService.deleteMenu(menuId);
 	}
 
 }
