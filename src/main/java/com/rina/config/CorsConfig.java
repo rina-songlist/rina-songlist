5bd1c9d63198ae600ebdecbbc9fe9ab5b8a2ed8b
ArvinXY <arvinjunior@163.com>
2022-03-26 18:45:42 +0900

    fix(config): 更改了跨域相关的配置类
    修复了跨域连接失败的问题

--- src/main/java/com/rina/config/CorsConfig.java
+++ src/main/java/com/rina/config/CorsConfig.java
@@ -1,6 +1,10 @@
 package com.rina.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,7 +21,27 @@ public class CorsConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedHeaders("*")
-				.allowedMethods("POST", "GET")
-				.allowedOrigins("*");
+				.allowedMethods("POST", "GET", "DELETE", "PUT")
+				.allowedOrigins("*")
+				.exposedHeaders("*")
+				.allowCredentials(true).maxAge(3600);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig());
+		return new CorsFilter(source);
+	}
+
+	private CorsConfiguration corsConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		// 请求常用的三种配置，*代表允许所有，也可以自定义属性（比如 header 只能带什么，只能是 post 方式等）
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setMaxAge(3600L);
+		return corsConfiguration;
 	}
 }
