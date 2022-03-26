3be8ece1fb97e795d3f99100e5d0f824a7790a43
ArvinXY <arvinjunior@163.com>
2022-03-26 20:51:17 +0900

    fix(config): 更改了跨域相关的配置类
    修复了跨域连接失败的问题

--- src/main/java/com/rina/config/CorsConfig.java
+++ src/main/java/com/rina/config/CorsConfig.java
@@ -1,7 +1,10 @@
 package com.rina.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -13,14 +16,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedHeaders("*")
-				.allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS", "HEAD")
-				.allowedOriginPatterns("*")
-				.exposedHeaders("*")
-				.allowCredentials(true).maxAge(3600);
+	/**
+	 * 跨域配置
+	 */
+	@Bean
+	public CorsFilter corsFilter()
+	{
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		// 设置访问源地址
+		config.addAllowedOriginPattern("*");
+		// 设置访问源请求头
+		config.addAllowedHeader("*");
+		// 设置访问源请求方法
+		config.addAllowedMethod("*");
+		// 有效期 1800秒
+		config.setMaxAge(3600L);
+		// 添加映射路径，拦截一切请求
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		// 返回新的CorsFilter
+		return new CorsFilter(source);
 	}
 
 }
