e7434b5843a74094faba2e452b4ee0a1bf31a249
ArvinXY <arvinjunior@163.com>
2022-03-26 19:37:36 +0900

    fix(config): 更改了跨域相关的配置类
    修复了跨域连接失败的问题

--- src/main/java/com/rina/config/CorsConfig.java
+++ src/main/java/com/rina/config/CorsConfig.java
@@ -40,6 +40,7 @@ public class CorsConfig {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		// 请求常用的三种配置，*代表允许所有，也可以自定义属性（比如 header 只能带什么，只能是 post 方式等）
 		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedOriginPattern("*");
 		corsConfiguration.addAllowedHeader("*");
 		corsConfiguration.addAllowedMethod("*");
 		corsConfiguration.setAllowCredentials(true);
