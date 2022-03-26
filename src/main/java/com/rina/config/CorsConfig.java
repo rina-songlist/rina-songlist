345751e864e6f14af2a301bfb1f7811c8c992b7a
ArvinXY <arvinjunior@163.com>
2022-03-26 20:32:54 +0900

    fix(config): 更改了跨域相关的配置类
    修复了跨域连接失败的问题

--- src/main/java/com/rina/config/CorsConfig.java
+++ src/main/java/com/rina/config/CorsConfig.java
@@ -17,31 +17,10 @@ public class CorsConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 				.allowedHeaders("*")
-				.allowedMethods("POST", "GET", "DELETE", "PUT")
+				.allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS", "HEAD")
 				.allowedOriginPatterns("*")
 				.exposedHeaders("*")
 				.allowCredentials(true).maxAge(3600);
 	}
 
-//	@Bean
-//	public FilterRegistrationBean corsFilter() {
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/**", corsConfig());
-//		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-//		//*****这里设置了优先级*****
-//		bean.setOrder(1);
-//		return bean;
-//	}
-//
-//	private CorsConfiguration corsConfig() {
-//		CorsConfiguration corsConfiguration = new CorsConfiguration();
-//		// 请求常用的三种配置，*代表允许所有，也可以自定义属性（比如 header 只能带什么，只能是 post 方式等）
-//		corsConfiguration.addAllowedOrigin("*");
-//		corsConfiguration.addAllowedOriginPattern("*");
-//		corsConfiguration.addAllowedHeader("*");
-//		corsConfiguration.addAllowedMethod("*");
-//		corsConfiguration.setAllowCredentials(true);
-//		corsConfiguration.setMaxAge(3600L);
-//		return corsConfiguration;
-//	}
 }
