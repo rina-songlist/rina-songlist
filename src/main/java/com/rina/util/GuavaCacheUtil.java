854ffef5158eeec6b4a5f467fc9c3f4ea0eaba17
ArvinXY <arvinjunior@163.com>
2022-06-11 12:07:35 +0900

    refactor(util): 更改了guava cache的工具类
    使其通用化

--- src/main/java/com/rina/util/GuavaCacheUtil.java
+++ src/main/java/com/rina/util/GuavaCacheUtil.java
@@ -1,22 +1,23 @@
 package com.rina.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.rina.config.AppProperties;
+import com.rina.exception.GuavaCacheDataNotExistException;
+import com.rina.exception.GuavaCacheNullKeyException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 把用户信息通过Guava Cache缓存
- *
+ * guava cache的工具类
  * @author arvin
  * @date 2022/03/21
  */
@@ -25,42 +26,75 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class GuavaCacheUtil {
 
-	private final AppProperties appProperties;
+	private final CommonUtil commonUtil;
 
-	private LoadingCache<String, Map<String, String>> cache = null;
+	private LoadingCache<String, String> cache = null;
 
 	@PostConstruct
 	public void init() {
-		long expireTime = 1L;
-		final String[] strings = appProperties.getJwt().getTokenExpireTime().split("\\*");
-		for (String string : strings) {
-			expireTime *= Long.parseLong(string);
-		}
-
 		cache = CacheBuilder.newBuilder()
 				.maximumSize(10)
-				.expireAfterWrite(expireTime, TimeUnit.MILLISECONDS)
+				.expireAfterWrite(commonUtil.tokenExpireMillis(), TimeUnit.MILLISECONDS)
 				.build(
-						new CacheLoader<String, Map<String, String>>() {
+						new CacheLoader<String, String>() {
 							@Override
-							public HashMap<String, String> load(String key) throws Exception {
+							public String load(String key) throws Exception {
 								return null;
 							}
 						}
 				);
 	}
 
-	public void put(String token, Map<String, String> userDetails) {
-		Optional.ofNullable(get(token))
-				.orElseGet(() -> {
-					this.cache.put(token, userDetails);
-					return null;
-				});
-		log.info("当前用户已存入cache");
+	/**
+	 * 存入缓存
+	 * @param key 键
+	 * @param value 值
+	 * @param <T> 值的数据类型
+	 * @throws JsonProcessingException json序列化错误
+	 * @throws GuavaCacheNullKeyException 键为空值
+	 */
+	public<T> void put(String key, T value) throws JsonProcessingException {
+		if (StringUtils.isNotEmpty(key) && ObjectUtils.isNotEmpty(value)) {
+			this.cache.put(key, CommonUtil.objectMapper().writeValueAsString(value));
+			log.info("当前用户已存入cache");
+			return;
+		}
+		if (StringUtils.isEmpty(key)) {
+			log.error("缓存参数有误，key：{}，value：{}", key, value);
+			throw new GuavaCacheNullKeyException();
+		}
+
 	}
 
-	public Map<String, String> get(String token) {
-		return this.cache.getIfPresent(token);
+	/**
+	 * 获取缓存
+	 * @param key 键
+	 * @return 键所对应的值
+	 * @param <T> 值的数据类型
+	 * @throws JsonProcessingException json序列化错误
+	 * @throws GuavaCacheDataNotExistException 值不存在
+	 */
+	public<T> T get(String key) throws JsonProcessingException {
+		final String stringValue = this.cache.getIfPresent(key);
+
+		if (stringValue == null) {
+			throw new GuavaCacheDataNotExistException("当前数据不存在！");
+		}
+		return CommonUtil.objectMapper().readValue(stringValue, new TypeReference<T>() {
+		});
+	}
+
+	/**
+	 * 删除键
+	 * @param key 键
+	 * @throws GuavaCacheNullKeyException 键为空值
+	 */
+	public void delete(String key) {
+		if (StringUtils.isEmpty(key)) {
+			throw new GuavaCacheNullKeyException();
+		}
+
+		this.cache.invalidate(key);
 	}
 
 }
