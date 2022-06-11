9b0a797e25e477aff4de3f69789907570b82e135
ArvinXY <arvinjunior@163.com>
2022-06-11 11:15:40 +0900

    feat(service): 添加了UserDetailsService的实现类
    添加了UserDetailsService的实现类

---
+++ src/main/java/com/rina/service/Impl/UserDetailsServiceImpl.java
@@ -0,0 +1,52 @@
+package com.rina.service.Impl;
+
+import com.rina.domain.LoginUser;
+import com.rina.domain.User;
+import com.rina.mapper.RolePermissionMapper;
+import com.rina.mapper.RoleUserMapper;
+import com.rina.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * UserDetailsService的实现类
+ * @author arvin
+ * @date 2022/05/30
+ */
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UserMapper userMapper;
+	private final RoleUserMapper roleUserMapper;
+	private final RolePermissionMapper rolePermissionMapper;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		final User user = userMapper.login(username);
+		if (Objects.isNull(user)) {
+			log.info("此用户不存在：{}",username);
+			throw new UsernameNotFoundException("用户名或密码错误");
+		}
+
+		final Long roleId = roleUserMapper.findRoleByUser(user.getId()).getRoleId();
+		List<String> permissions = new ArrayList<>();
+		rolePermissionMapper.findPermissionsByRoleId(roleId)
+				.forEach(rolePermission -> {
+					permissions.add(rolePermission.getPermission().getName());
+				});
+
+
+		return new LoginUser(user, roleId, permissions);
+	}
+
+}
