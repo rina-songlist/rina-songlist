15482866f63321609fd2d6001050aa06bb20fbe7
ArvinXY <arvinjunior@163.com>
2022-06-11 12:06:16 +0900

    feat(util): 添加了全局常量类
    添加了全局常量类

---
+++ src/main/java/com/rina/util/Constants.java
@@ -0,0 +1,32 @@
+package com.rina.util;
+
+/**
+ * 全局常量类
+ * @author arvin
+ * @date 2022/06/03
+ */
+public class Constants {
+
+	/**
+	 * 缓存类型为redis
+	 */
+	public static final String CACHE_REDIS = "redis";
+
+	/**
+	 * 缓存类型为guava
+	 */
+	public static final String CACHE_GUAVA = "guava";
+
+	/* --- */
+	/**
+	 * 登陆用户的缓存前缀
+	 */
+	public static final String LOGIN_CACHE_PREFIX = "login:";
+
+	/* --- */
+	/**
+	 *前端数据空值
+	 */
+	public static final String FRONTEND_NULL_STRING = "undefined";
+
+}
