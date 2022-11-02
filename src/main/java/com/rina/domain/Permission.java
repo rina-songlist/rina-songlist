4674ad814212204d9836fec68c293ad22d06a693
ArvinXY <arvinjunior@163.com>
2022-11-03 04:18:50 +0900

    feat(domain): 更改了permission表所对应的实体类
    添加了新字段所对应的变量

--- src/main/java/com/rina/domain/Permission.java
+++ src/main/java/com/rina/domain/Permission.java
@@ -8,6 +8,7 @@ import java.util.Date;
 /**
  * permission表所对应的实体类
  * @author arvin
+ * @date 2022/10/10
  */
 @Data
 @Builder
@@ -23,6 +24,16 @@ public class Permission implements Serializable {
     private String name;
 
     private String remark;
+
+    private Long parentId;
+
+    private Long orderValue;
+
+    private Boolean deepest;
+
+    private Boolean disabled;
+
+    private Long mainPermissionId;
 
     private String createBy;
 
